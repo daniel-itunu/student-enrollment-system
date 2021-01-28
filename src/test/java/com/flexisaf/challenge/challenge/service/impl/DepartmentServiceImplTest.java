72cf79bbc9d57face0744e640109ae1e5e6712a7
Daniel Itunu <danielsoft4ever@gmail.com>
2021-01-28 23:14:41 +0000

    update tests

--- src/test/java/com/flexisaf/challenge/challenge/service/impl/DepartmentServiceImplTest.java
+++ src/test/java/com/flexisaf/challenge/challenge/service/impl/DepartmentServiceImplTest.java
@@ -1,9 +1,7 @@
 package com.flexisaf.challenge.challenge.service.impl;
 
 import com.flexisaf.challenge.challenge.dto.DepartmentDto;
-import com.flexisaf.challenge.challenge.dto.StudentDto;
 import com.flexisaf.challenge.challenge.model.Department;
-import com.flexisaf.challenge.challenge.model.Student;
 import com.flexisaf.challenge.challenge.repository.DepartmentRepository;
 import com.flexisaf.challenge.challenge.repository.StudentRepository;
 import com.flexisaf.challenge.challenge.service.DepartmentService;
@@ -12,9 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.stereotype.Service;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -37,20 +34,27 @@ class DepartmentServiceImplTest {
         Department department = new Department();
         department.setName("biology");
         department.setId("bio");
-
         DepartmentDto departmentDto = new DepartmentDto();
         departmentDto.setName("biology");
         departmentDto.setId("bio");
-
         when(departmentRepository.save(any(Department.class))).thenReturn(department);
         assertEquals("department added", departmentService.addDepartment(departmentDto));
     }
 
     @Test
-    void findDepartmentByName() {
+    void findDepartmentByName() throws Exception {
+        Department department = new Department();
+        department.setName("biology");
+        when(departmentRepository.findDepartmentByName("biology")).thenReturn(department);
+        assertEquals("biology", departmentService.findDepartmentByName("biology").getName());
     }
 
     @Test
-    void retrieveDepartments() {
+    void retrieveDepartments() throws Exception {
+        Department department = new Department();
+        department.setName("biology");
+        department.setId("bio");
+        when(departmentRepository.findAll()).thenReturn(List.of(department));
+        assertEquals(1, departmentService.retrieveDepartments().size());
     }
 }
---
+++ src/test/java/com/flexisaf/challenge/challenge/service/impl/StudentSearchServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.flexisaf.challenge.challenge.service.impl;
+
+import com.flexisaf.challenge.challenge.model.Student;
+import com.flexisaf.challenge.challenge.repository.StudentRepository;
+import com.flexisaf.challenge.challenge.service.StudentSearchService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class StudentSearchServiceImplTest {
+
+    @Autowired
+    private StudentSearchService studentSearchService;
+
+    @MockBean
+    private StudentRepository studentRepository;
+
+    @Test
+    void getSearchResult() throws Exception {
+        Student student = new Student();
+        student.setFirstName("name");
+        when(studentRepository.findAll()).thenReturn(List.of(student));
+        assertEquals("name", studentSearchService.getSearchResult("name").get(0).getFirstName());
+    }
+}
